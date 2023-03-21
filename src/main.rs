b2d4860fbadf18c5ad5189f010d2cfb612f64100
Vitoso7 <victorhugodelisboa@gmail.com>
2023-03-21 11:41:47 -0300

    printing start of material

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,14 @@
+use nom::{
+    bytes::complete::{tag, take_until},
+    sequence::delimited,
+    IResult,
+};
 use std::io::BufRead;
 use std::process::Stdio;
 use std::{env, process::Command};
 
-const FRAME_RATE: f32 = 29.97;
+// Crate to calculate timecode
+use vtc::{rates, Timecode};
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -14,103 +20,131 @@ fn main() {
 fn run_ffmpeg_cmd(args: &Vec<String>) {
     let file_input_arg = &args[1];
 
-    // TODO Convert frame to time example: 209 frame === 6.29
-    // TODO Stop process when all needed values is found
-    // TODO try using list of .arg
-    // TODO identify the blackdetect data that has at least 5s and is at the start of the video
-
-    // ffmpeg -hide_banner -i
-    // videos-promo-test/ARQUIVO_COMERCIAL_TESTE_ORIGINAL_8CH.mxf -vf
-    // blackframe,blackdetect -f null -
-
-    // ffprobe -hide_banner -f lavfi -i "movie=videos-promo-test/ARQUIVO_C
-    // OMERCIAL_TESTE_ORIGINAL_8CH.mxf,blackframe,blackdetect" -show_entries frame=pkt_pts -of default=nw=1
-
-    // Running FFMPEG on Docker
-    let mut ffprobe_cmd = Command::new("docker")
-        .args(["exec", "e99da464f45d", "ffmpeg", "-hide_banner", "-i"])
-        .arg(format!("/{}", file_input_arg))
-        .args(["-vf", "blackframe,blackdetect", "-f", "null", "-"])
+    let mut ffmpeg_cmd = Command::new("ffmpeg")
+        .args(["-hide_banner", "-i"])
+        .arg(file_input_arg)
+        .args(["-vf", "blackdetect,blackframe", "-f", "null", "-"])
         .stderr(Stdio::piped())
         .spawn()
         .expect("error running ffprobe command");
 
-    let stderr = ffprobe_cmd.stderr.take().unwrap();
-
+    let stderr = ffmpeg_cmd.stderr.take().unwrap();
     let stderr_reader = std::io::BufReader::new(stderr);
 
+    let mut prev_line = String::new();
     for line in stderr_reader.lines() {
         let buf_line = line.expect("Failed to read line from stdout");
-        println!("{}", buf_line);
         if buf_line.contains("blackdetect") {
-            ffprobe_cmd.kill().expect("failed to kill ffmpeg process");
-            calc_timecode_by_frame(210, FRAME_RATE);
-            calc_timecode_by_timestamp(4.004, FRAME_RATE as f64);
+            ffmpeg_cmd.kill().expect("failed to kill ffmpeg process");
+
+            match extract_filter_prefix(&mut prev_line) {
+                Ok((f, _)) => match get_filter_value(f, "frame") {
+                    Some(v) => {
+                        let timecode = get_timecode(v as u32);
+                        println!("{}", timecode)
+                    }
+                    None => panic!("nothing found"),
+                },
+                Err(_) => panic!("Error removing prefix"),
+            }
+
+            get_timecode(210);
         }
+        prev_line = buf_line;
     }
 }
 
-fn calc_timecode_by_frame(frame: u16, frame_rate: f32) -> String {
-    let total_elapsed_time = frame as f32 / frame_rate;
-    println!("{}", total_elapsed_time);
+fn get_filter_value(raw_str: &str, value: &str) -> Option<f32> {
+    for part in raw_str.split_whitespace() {
+        let mut split_iter = part.split(':');
+        let key = split_iter.next()?;
+        let value_str = split_iter.next()?;
 
-    let hours = (total_elapsed_time / 3600.0).floor();
-    let minutes = ((total_elapsed_time - (hours * 3600.0)) / 60.0).floor();
-    let seconds = (total_elapsed_time - (hours * 3600.0) - (minutes * 60.0)).floor();
-    let frames =
-        ((total_elapsed_time - (hours * 3600.0) - (minutes * 60.0) - seconds) * frame_rate).round();
+        if key == value {
+            return value_str.parse().ok();
+        }
+    }
 
-    let timecode = format!("{:02}:{:02}:{:02}:{:02}", hours, minutes, seconds, frames);
-
-    println!("timecode from frame = {}", timecode);
-
-    return timecode;
+    return None;
 }
 
-fn calc_timecode_by_timestamp(timestamp: f64, frame_rate: f64) -> String {
-    let hours = (timestamp / 3600.0).floor();
-    let minutes = ((timestamp / 60.0) % 60.0).floor();
-    let seconds = (timestamp % 60.0).floor();
-    let frames = ((timestamp * frame_rate) % frame_rate).round();
+fn extract_filter_prefix(input: &mut String) -> IResult<&str, ()> {
+    let (parsed_value, _) = delimited(tag("["), take_until("] "), tag("] "))(input.as_str())?;
+    return Ok((parsed_value, ()));
+}
 
-    let timecode = format!("{:02}:{:02}:{:02}:{:02}", hours, minutes, seconds, frames);
-
-    println!("timecode from timestamp = {}", timecode);
-
-    return timecode;
+fn get_timecode(frame: u32) -> String {
+    return Timecode::with_frames(frame, rates::F29_97_DF)
+        .unwrap()
+        .timecode();
 }
 
 #[cfg(test)]
 mod tests {
-    use crate::{calc_timecode_by_frame, calc_timecode_by_timestamp, FRAME_RATE};
+    use crate::{extract_filter_prefix, get_filter_value, get_timecode};
 
     #[test]
-    fn calc_timecode_frame_209() {
-        let timecode = calc_timecode_by_frame(209, FRAME_RATE);
-        assert_eq!(timecode, "00:00:06:29");
+    fn get_framerate_vtc_lib_frame_209() {
+        let timecode = get_timecode(209);
+        assert_eq!(timecode, "00:00:06;29");
     }
 
     #[test]
-    fn calc_timecode_frame_210() {
-        let timecode = calc_timecode_by_frame(210, FRAME_RATE);
-        assert_eq!(timecode, "00:00:07:00");
+    fn get_framerate_vtc_lib_frame_210() {
+        let timecode = get_timecode(210);
+        assert_eq!(timecode, "00:00:07;00");
     }
 
     #[test]
-    fn calc_timecode_by_timestamp_6_973633() {
-        let timecode = calc_timecode_by_timestamp(6.973633, FRAME_RATE as f64);
-        assert_eq!(timecode, "00:00:06:29");
+    fn get_blackframe_filter_values() {
+        let mut test_str = String::from("[Parsed_blackframe_0 @ 0x10e60fd50] frame:720 pblack:100 pts:720 t:24.024000 type:I last_keyframe:720");
+        let filter_value = extract_filter_prefix(&mut test_str);
+        assert_eq!(
+            filter_value,
+            Ok((
+                "frame:720 pblack:100 pts:720 t:24.024000 type:I last_keyframe:720",
+                ()
+            ))
+        )
     }
 
     #[test]
-    fn calc_timecode_by_timestamp_7_007() {
-        let timecode = calc_timecode_by_timestamp(7.007, FRAME_RATE as f64);
-        assert_eq!(timecode, "00:00:07:00")
+    fn get_blackdetect_filter_values() {
+        let mut test_str = String::from("[blackdetect @ 0x13e00d040] black_start:4.97163 black_end:7.007 black_duration:2.03537");
+        let filter_value = extract_filter_prefix(&mut test_str);
+        assert_eq!(
+            filter_value,
+            Ok((
+                "black_start:4.97163 black_end:7.007 black_duration:2.03537",
+                ()
+            ))
+        )
     }
 
     #[test]
-    fn calc_timecode_by_timestamp_4_004() {
-        let timecode = calc_timecode_by_timestamp(4.004, FRAME_RATE as f64);
-        assert_eq!(timecode, "00:00:04:00");
+    fn get_filter_value_black_end() {
+        let value = get_filter_value(
+            "black_start:4.97163 black_end:7.007 black_duration:2.03537",
+            "black_end",
+        );
+        assert_eq!(value, Some(7.007));
+    }
+
+    #[test]
+    fn get_filter_value_frame() {
+        let value = get_filter_value(
+            "frame:720 pblack:100 pts:720 t:24.024000 type:I last_keyframe:720",
+            "frame",
+        );
+        assert_eq!(value, Some(720.0));
+    }
+
+    #[test]
+    fn get_filter_value_none() {
+        let value = get_filter_value(
+            "frame:720 pblack:100 pts:720 t:24.024000 type:I last_keyframe:720",
+            "nothing",
+        );
+        assert_eq!(value, None);
     }
 }
